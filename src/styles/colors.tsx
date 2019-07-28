cfcdcb719b1d1456b2c954eee20fe7359f343d8a
vbertazzo <vbertazzo@gmail.com>
2019-07-28 20:03:43 -0300

    chore: Add global styles

---
+++ src/styles/colors.tsx
@@ -0,0 +1,23 @@
+export default {
+  transparent: 'transparent',
+
+  black: '#000',
+  white: '#fff',
+  error: '#c53030',
+  success: '#2f855a',
+  first: '#cc0067',
+  second: '#0f0e11',
+  third: '#e21212',
+
+  gray: {
+    100: '#f7fafc',
+    200: '#edf2f7',
+    300: '#e2e8f0',
+    400: '#cbd5e0',
+    500: '#a0aec0',
+    600: '#718096',
+    700: '#4a5568',
+    800: '#2d3748',
+    900: '#1a202c',
+  },
+};
---
+++ src/styles/global.tsx
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+
+import colors from './colors';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  ::selection {
+    background: ${colors.first};
+    color: #fff;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    background: ${colors.second};
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  #root {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
